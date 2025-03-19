496ef1c3a763bf40df570992e7a378d19c45c682
Senapp Studios <senappstudios@gmail.com>
2025-03-19 22:58:42 +0900

    TextInput fix and link fix

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -21,7 +21,7 @@ export const HomePage: React.FC = () => {
             <div className={!mobileView ? css.introductionContainer : css.mobileIntroduction}>
                 <div>
                     <span>{GetTranslation(TranslationIdentity.Introduction1)}</span>
-                    <a href={toUrl(`/pages/resume`)} className={css.introductionName}>{GetTranslation(TranslationIdentity.Name)}</a>
+                    <a href={toUrl(`/pages/contact`)} className={css.introductionName}>{GetTranslation(TranslationIdentity.Name)}</a>
                     <span>{GetTranslation(TranslationIdentity.Introduction2)}</span>
                 </div>
                 <div>
